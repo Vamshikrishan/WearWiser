9bdc7d113727667a0e4c63eb88b372436d9e1312
Vamshi Krishna <145258844+Vamshikrishan@users.noreply.github.com>
2026-02-01 09:45:33 +0000

    sucessfully added logo and changed colour

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/landing.css";
-import logo from "../assets/landing/backgroundimg.png";
+import logo from "../assets/landing/logo.jpeg";
 
 export default function Navbar() {
   return (
