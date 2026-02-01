afe8a4879c185117343ff8aa9cfd9fe3ab07270e
Vamshi Krishna <145258844+Vamshikrishan@users.noreply.github.com>
2026-02-01 07:26:42 +0000

    add ing basic code

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import "../styles/landing.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2 className="logo">WearWiser</h2>
+
+      <div className="nav-buttons">
+        <button className="btn-outline">Login</button>
+        <button className="btn-primary">Sign Up</button>
+      </div>
+    </nav>
+  );
+}
---
+++ src/pages/landing.jsx
@@ -0,0 +1,33 @@
+import Navbar from "../components/Navbar";
+import "../styles/landing.css";
+
+export default function Landing() {
+  return (
+    <>
+      <Navbar />
+
+      <section className="hero">
+        <h1>AI Fashion Stylist</h1>
+        <p>
+          Discover outfits that perfectly match your body type, lifestyle,
+          and personality.
+        </p>
+
+        <div className="hero-buttons">
+          <button className="btn-primary">Get Started</button>
+          <button className="btn-outline">Continue as Guest</button>
+        </div>
+      </section>
+
+      <section className="images">
+        <img src="/hero1.jpg" alt="fashion" />
+        <img src="/hero2.jpg" alt="fashion" />
+        <img src="/hero3.jpg" alt="fashion" />
+      </section>
+
+      <footer>
+        © 2026 WearWiser – AI Fashion Stylist
+      </footer>
+    </>
+  );
+}
