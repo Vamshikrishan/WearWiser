0e077f64d141ff6defb9e9b0929e0b5d2c3c407f
Vamshi Krishna <145258844+Vamshikrishan@users.noreply.github.com>
2026-02-04 20:46:22 +0000

    removing the api key for securite reasuons

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCDbDgg4bCW3vf3Hpob7Kqq6MN2nmg3bsg",
-  authDomain: "wearwiser-d8933.firebaseapp.com",
-  projectId: "wearwiser-d8933",
-  storageBucket: "wearwiser-d8933.appspot.com",
-  messagingSenderId: "831197458432",
-  appId: "1:831197458432:web:4c7957973ce1dc29a3e8e5"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
